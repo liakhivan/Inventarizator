36e42a18cafe71440a3fe8e9e465c7cab54c18a3
liakhivan21@gmail.com <liakhivan21@gmail.com>
2019-07-26 00:43:26 +0300

    Add project files.

---
+++ InventarizatorBL/Model/ComponentOfProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventarizatorBL.Model
+{
+    class ComponentOfProduct
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int IngredientId { get; set; }
+        public int AmountOfIngredient { get; set; }
+
+    }
+}
---
+++ InventarizatorBL/Model/Ingredient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventarizatorBL.Model
+{
+    class Ingredient
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
