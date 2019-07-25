75861508d6f34743bee331a51dfb08396224e9dc
liakhivan21@gmail.com <liakhivan21@gmail.com>
2019-07-26 01:02:20 +0300

    Create class entity package.

---
+++ InventarizatorBL/Model/Package.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventarizatorBL.Model
+{
+    class Package
+    {
+        [Key]
+        public int Id { get; set; }
+        public double Weight { get; set; }
+    }
+}
